504b9ba8797b306df96c781e867a25f0c80992e3
Bruce Harris <bruce.harris@weareadaptive.com>
2023-12-19 17:33:03 -0500

    fix resetting state

--- src/state.ts
+++ src/state.ts
@@ -1,14 +1,18 @@
-import { Observable, map, mergeWith, scan, startWith } from 'rxjs'
+import { Observable, mergeWith, scan, startWith } from 'rxjs'
 import { pricesDto$, resetPrices$ } from './service'
+import { Price } from './model'
 
 export const prices$: Observable<Record<string, number>> = pricesDto$.pipe(
+  mergeWith(resetPrices$),
   scan(
-    (accum, current) => ({
-      ...accum,
-      [current.symbol]: current.price,
-    }),
+    (accum, current: Price | void) =>
+      !current
+        ? {}
+        : {
+            ...accum,
+            [current.symbol]: current.price,
+          },
     {}
   ),
-  mergeWith(resetPrices$.pipe(map(() => ({})))),
   startWith({})
 )
