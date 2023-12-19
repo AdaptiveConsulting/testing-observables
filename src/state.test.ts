e82ff5d1ef4a00bf816f61f7e0d321733ff381c1
Bruce Harris <bruce.harris@weareadaptive.com>
2023-12-19 15:21:49 -0500

    add assertion on price update

--- src/state.test.ts
+++ src/state.test.ts
@@ -34,6 +34,25 @@ describe('prices$', () => {
     expect(latestEmission()).toEqual({})
   })
 
+  it('should emit object containing latest prices after pricesDto$ emits', () => {
+    // call next() on the subject that mocks out the source observable
+    // priceDto$ that the observable under test depends on to simulate
+    // that observable emitting prices, and ensure the new price is
+    // emitted as expected in the observable under test
+    mockPricesDto$.next({ symbol: 'XOM', price: 48.17 })
+    expect(latestEmission()).toEqual({ XOM: 48.17 })
+
+    // add another instrument/price, ensure both instruments
+    // appear in the resulting emission
+    mockPricesDto$.next({ symbol: 'BA', price: 218.93 })
+    expect(latestEmission()).toEqual({ XOM: 48.17, BA: 218.93 })
+
+    // update the price of the first instrument, ensure the price is
+    // updated in the resulting emission
+    mockPricesDto$.next({ symbol: 'XOM', price: 48.21 })
+    expect(latestEmission()).toEqual({ XOM: 48.21, BA: 218.93 })
+  })
+
   it('should not error', () => {
     expect(error).not.toBeCalled()
   })
